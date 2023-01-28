0751ce86d3cbd63ed20a96371cb4bc248606ef89
Vlada <80097541+Rafaelkaaa@users.noreply.github.com>
2023-01-28 18:26:37 +0200

    Add App class

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import DBservices.Database;
+import DBservices.DatabaseQueryService;
+
+import java.sql.SQLException;
+
+
+public class App {
+    public static void main(String[] args) throws SQLException {
+        Database database = Database.getINSTANCE();
+        System.out.println(new DatabaseQueryService()
+                .findMaxProjectsClient(database, "SQL/find_max_projects_client.sql"));
+    }
+}
