e73ecb0a32f7ad2e6c265884d403ec76c435f6fb
Vlada <80097541+Rafaelkaaa@users.noreply.github.com>
2023-01-28 18:31:02 +0200

    Add model class

---
+++ src/main/java/Model/MaxProjectCountClient.java
@@ -0,0 +1,16 @@
+package Model;
+
+import lombok.Setter;
+
+public class MaxProjectCountClient {
+    private String name;
+    private int projectCount;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setProjectCount(int projectCount) {
+        this.projectCount = projectCount;
+    }
+}
